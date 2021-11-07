ced5a73bc046a95ab7b71b6c1568ef9f926fd1ec
jadonwellman <jadon.wellman@gmail.com>
2021-11-06 17:17:57 -0700

    Added further understanding

--- Chi-Squared Distribution.R
+++ Chi-Squared Distribution.R
@@ -2,13 +2,17 @@
 
 set.seed(123)
 
-intV <- 10
+# The v (Nu), degrees of freedom. Compare 5 and 50. 5 should be skewed to the
+# right. 50+ should be ~normal
+intV <- 50
+
 vPopulation <- rnorm(1000, mean=0, sd=1)
 nMean <- mean(vPopulation)
 nSD <- sd(vPopulation)
 
 set_info <- function (vInput,strTitle) {
-  hist(vInput,main = paste('Histogram of' , strTitle))
+  hist(vInput,main = paste('Histogram of' , strTitle),
+       xlab=paste(strTitle,'Values'))
   print(paste('Mean:',mean(vInput)))
   print(paste('Variance:',var(vInput)))
   print(paste('SD:',sd(vInput)))
@@ -19,10 +23,21 @@ set_info(vPopulation,'Population')
 vSumZSquared <- c()
 for (i in 1:1000) {
   vSample <- sample(vPopulation, size=intV, replace = T)
+  
+  # Since the mean ~0 and the sd ~1, vZ should be pretty much a copy of 
+  # the sample
   vZ <- sapply(vSample,function(x) (x-nMean)/nSD)
+  
+  # Squaring the values will widen the spread and make everything >=0
   vZSquared <- sapply(vZ, function(x) x^2)
+  
+  # Summing them up should make the spread even greater, and has a 
+  # relationship to the size of the sample, the intV. As intV increases
+  # there is room to breathe on the left side of the mean, so it gets
+  # more normally distributed
   vSumZSquared[i] <- sum(vZSquared)
 }
 
-# mean should be "v"(intV), and variance "2v" (2*intV)
+# Mean should be "v"(intV), and variance "2v" (2*intV)
+# I'm thinking the squared part of X^2 comes from the Z "squared"
 set_info(vSumZSquared,'Sum Z Squared') 
